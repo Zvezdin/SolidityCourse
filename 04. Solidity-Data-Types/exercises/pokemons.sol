bc7c264404ea526c31b03d04f7613f7150d5a0bd
zvezdin <git@zvezd.in>
2018-02-25 23:34:27 +0200

    Added pokemon game solution

---
+++ 04. Solidity-Data-Types/exercises/pokemons.sol
@@ -0,0 +1,50 @@
+pragma solidity 0.4.20;
+
+contract Pokemons {
+    //a few defined pokemons
+    enum Pokemon { Bulbasaur, Ivysaur, Venusaur, Charmander, Charmeleon, Charizard, Squirtle, Wartortle,
+    Blastoise, Caterpie, Metapod, Butterfree, Weedle, Kakuna, Beedrill, Pidgey, Pidgeotto, Pidgeot, Rattata, Raticate,
+    Spearow, Fearow, Ekans, Arbok, Pikachu, Raichu, Sandshrew, Sandslash, NidoranF, Nidorina, Nidoqueen, NidoranM }
+    
+    //a pokemon can be caught at most once every 15 seconds if not caught already
+    modifier canPersonCatch(address person, Pokemon pokemon){
+        require(now > players[person].lastCatch + 15 seconds);
+        require(!hasPokemon(person, pokemon));
+        _;
+    }
+    
+    struct Player {
+        Pokemon[] pokemons;
+        uint lastCatch; //timestamp
+    }
+    
+    mapping(address => Player) players;
+    mapping(uint => address[]) pokemonHolders; //the key is uint256, because the amount of Pokemons may increase in the future
+    //and pass the uint8 range
+    
+    //the hash of the pokemon holder and the pokemon is the key. This allows constant time lookup of whether a pokemon is caught by a person
+    mapping(bytes32 => bool) hasPokemonMap;
+    
+    //returns if that person has caught a pokemon. It uses the hash of the address and the pokemon so that it can return the answer
+    //without loops
+    function hasPokemon(address person, Pokemon pokemon) internal view returns (bool) {
+        return hasPokemonMap[keccak256(person, pokemon)];
+    }
+    
+    function catchPokemon(Pokemon pokemon) public canPersonCatch(msg.sender, pokemon) {
+        players[msg.sender].pokemons.push(pokemon);
+        players[msg.sender].lastCatch = now;
+        
+        pokemonHolders[uint(pokemon)].push(msg.sender);
+        
+        hasPokemonMap[keccak256(msg.sender, pokemon)] = true;
+    }
+    
+    function getPokemonsByPerson(address person) public view returns (Pokemon[]) {
+        return players[person].pokemons;
+    }
+    
+    function getPokemonHolders(Pokemon pokemon) public view returns (address[]) {
+        return pokemonHolders[uint(pokemon)];
+    }
+}
